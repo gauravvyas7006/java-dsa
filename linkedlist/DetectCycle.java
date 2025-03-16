d62ed1a659b8d50ff3e9e14a6c8dfc8428b301ad
Gaurav Vyas <vyasgaurav7006@gmail.com>
2025-03-16 10:30:00 +0530

    Add DetectCycle solution

---
+++ linkedlist/DetectCycle.java
@@ -0,0 +1,17 @@
+public class DetectCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
