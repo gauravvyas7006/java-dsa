0f2c2e4589f51abdaa93fba10aa6a0b4a132fd0d
Gaurav Vyas <vyasgaurav7006@gmail.com>
2026-03-31 10:00:00 +0530

    Add LongestSubstringWithoutRepeat solution

---
+++ strings/LongestSubstringWithoutRepeat.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeat {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLen = 0, left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) left = Math.max(left, map.get(c) + 1);
+            map.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeat ls = new LongestSubstringWithoutRepeat();
+        System.out.println(ls.lengthOfLongestSubstring("abcabcbb")); // 3
+    }
+}
